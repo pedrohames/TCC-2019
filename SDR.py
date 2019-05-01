a930311fe79267987b72dbffe6b88b5a87dfba69
Pedro Hames <pedro.hamess@gmail.com>
2019-05-01 18:25:39 -0300

    Realizadas correções sugeridas em code review na classe DUT Classe SDR iniciada.

--- SDR.py
+++ SDR.py
@@ -1,54 +1,25 @@
-import SoapySDR
 import numpy as np
-
+from subprocess import PIPE, run
 
 class SDR:
 
-    def __init__(self, FS, BW, Fc):
+    def __init__(self):
         # Initialising HackRF One
-        self.hackrf = SoapySDR.Device(dict(driver='hackrf'))
+        self.driver = 'driver=hackrf'
 
-        self.FS = FS  # Sample frequency [Hz]
-        self.BW = BW  # Bandwidth [Hz]
-        self.Fc = Fc  # Central frequency  [Hz]
-
-        # HackRF Setup
-        self.hackrf.setSampleRate(SoapySDR.SOAPY_SDR_RX, 0, FS)
-        self.hackrf.setBandwidth(SoapySDR.SOAPY_SDR_RX, 1, BW)
-        self.hackrf.setFrequency(SoapySDR.SOAPY_SDR_RX, 1, Fc)
-        self.maxSamples = 131072
-
-    def receive(self, msec):
-        Nsamp = int(self.FS * msec/1000)  # Number of samples
-        assert Nsamp > self.maxSamples, f'Capture too short, must be more than {self.maxSamples*1000/self.FS} ms'
-
-        # Initialising capture stream, but still disable
-        rxStream = self.hackrf.setupStream(SoapySDR.SOAPY_SDR_RX, SoapySDR.SOAPY_SDR_CF32, [0])
-
-        # Activating capture stream
-        self.hackrf.activateStream(rxStream)
-
-        # Buffer
-        buffer = np.empty(Nsamp*2, np.complex64)
-        s_t = []
-
-        # Loop that capture the samples
-        while Nsamp > self.maxSamples:
-            sr = self.hackrf.readStream(rxStream, [buffer], buffer.size, timeoutUs=int(10e6))
-            assert sr.ret > 0, 'Error during capture'
-            Nsamp -= sr.ret
-            s_t.append(buffer[:sr.ret+1])
-        # After finishing this loop, the samples will be on s_t
-
-        # Disabling capture stream
-        self.hackrf.deactivateStream(rxStream)
-        # Closing capture stream
-        self.hackrf.closeStream(rxStream)
-
-        return s_t
+    def receive(self, fc, fs, msec, gain = None):
+        Nsamp = int(fs * msec / 1000)  # Number of samples
+        if gain is None:
+            command = f'./rx_sdr -f {int(fc)} -s {int(fs)} -d {self.driver} -n {Nsamp} -F CF32 samples.dat'
+        else:
+            command = f'./rx_sdr -f {int(fc)} -s {int(fs)} -d {self.driver} -n {Nsamp} -F CF32 -g {gain} samples.dat'
+            print(command)
+        run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+        return np.fromfile('samples.dat')
 
     def fft(self, s_t, n_fft):
         freqs = np.fft.fft(s_t, n_fft)
+        freqs[0] = 0
         return np.fft.fftshift(freqs)
 
     def max_freq_hold(self, s_f_list):
--- SDR_Test.py
+++ SDR_Test.py
@@ -2,22 +2,17 @@ import SDR
 import matplotlib.pyplot as plt
 import numpy as np
 
-Fc = 100.9e6
-Bw = 3e6
-Fs = 10e6
-msec = 10e3
-N_fft = 2048
+Fc = 5180e6
+Fs = 20e6
+msec = 4e3
+N_fft = 4096
 
-my_SDR = SDR.SDR(Fs, Bw, Fc)
+my_SDR = SDR.SDR()
 
-s_t_list = my_SDR.receive(msec)
+s_t = my_SDR.receive(Fc, Fs, msec)
+print(f'len(s_t) = {len(s_t)}')
 
-s_f_list = []
-for s_t in s_t_list:
-    s_f_list.append(my_SDR.fft(s_t, N_fft))
-
-
-max_freq = my_SDR.max_freq_hold(s_f_list)
-f = np.linspace(Fc-Bw/2, Fc+Bw/2, N_fft)
-plt.plot(f, max_freq)
+max_freq = my_SDR.fft(s_t, N_fft)
+f = np.linspace(Fc-Fs/2, Fc+Fs/2, N_fft)
+plt.plot(f/1e6, np.abs(max_freq))
 plt.show()
